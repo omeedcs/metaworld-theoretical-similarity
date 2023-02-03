a12dc427aaf8836a848661ea764f76a1c9c433b2
Arpan Dhatt <ary.dhatt@gmail.com>
2023-02-02 20:56:54 -0600

    use _setup_learn and collect_rollouts manually for testing (works)

--- stable-baselines-installation.py
+++ stable-baselines-installation.py
@@ -1,11 +1,25 @@
 import gym
 
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import *
 
 env = gym.make("CartPole-v1")
 
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10_000)
+# model.learn(total_timesteps=10_000)
+
+# manual learning
+total_timesteps = 10_000
+iteration = 0
+total_timesteps, callback = model._setup_learn(total_timesteps)
+while model.num_timesteps < total_timesteps:
+    continue_training = model.collect_rollouts(model.env, callback, model.rollout_buffer, n_rollout_steps=model.n_steps)
+
+    if not continue_training:
+        break
+
+    iteration += 1
+    model.train()
 
 vec_env = model.get_env()
 obs = vec_env.reset()
