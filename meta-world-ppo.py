7afa8f8fe16c676095b6b449986693a0f00bc08c
Arpan Dhatt <ary.dhatt@gmail.com>
2023-02-02 20:57:37 -0600

    Use _setup_learn and collect_rollouts to exit MW task early to prevent runtime error. (no error; unknown if policy converges)

--- meta-world-ppo.py
+++ meta-world-ppo.py
@@ -1,4 +1,5 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
 
 import metaworld
 import random
@@ -13,9 +14,53 @@ model = PPO("MlpPolicy", env, verbose=1)
 # we need to handle episode termination manually since SB3 doesn't issues #284 and #633
 # model.learn(total_timesteps=1000)
 
+class MWTerminationCallback(BaseCallback):
+    """
+    A custom callback that terminates the rollout procedure early when we've reached the maximum
+    steps of the metaworld environment.
+    """
+
+    def __init__(self, verbose = 0):
+        super(MWTerminationCallback, self).__init__(verbose)
+        self.step_count = 0
+
+    def _on_training_start(self):
+        pass
+
+    def _on_rollout_start(self):
+        pass
+
+    def _on_step(self) -> bool:
+        """
+        Returns false if we've reached the the maximimum number of steps for this MetaWorld environment
+        """
+        self.step_count += 1
+        if self.step_count > 500:
+            return False
+        return True
+
+    def _on_rollout_end(self):
+        pass
+
+    def _on_training_end(self):
+        pass
+
+# manual learning
+total_timesteps = 10_000
+iteration = 0
+total_timesteps, callback = model._setup_learn(total_timesteps, callback=MWTerminationCallback())
+while model.num_timesteps < total_timesteps:
+    continue_training = model.collect_rollouts(model.env, callback, model.rollout_buffer, n_rollout_steps=model.n_steps)
+
+    if not continue_training:
+        break
+
+    iteration += 1
+    model.train()
+
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(1000):
+for i in range(500):
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     # seems to render to an offscreen window which we can't see
