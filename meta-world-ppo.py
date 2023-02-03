88fefd13fe99202a95c207d5e2f9385d783508cc
Arpan Dhatt <ary.dhatt@gmail.com>
2023-02-03 05:26:23 -0600

    evaluate over multiple episodes; render to gif method; allowed rollout over multiple episodes (might be unsound)

--- meta-world-ppo.py
+++ meta-world-ppo.py
@@ -1,16 +1,26 @@
-from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import BaseCallback
-
-import metaworld
+from typing import Callable
 import random
 
-ml1 = metaworld.ML1('pick-place-v2')
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+import metaworld
+import imageio
+import numpy as np
 
-env = ml1.train_classes['pick-place-v2']()
-task = random.choice(ml1.train_tasks)
+task_name = 'pick-place-v2'
+
+ml1 = metaworld.ML1(task_name)
+
+env = ml1.train_classes[task_name]()
+task = ml1.train_tasks[0]
 env.set_task(task)
 
-model = PPO("MlpPolicy", env, verbose=1, n_steps=500, batch_size=50)
+params = {
+    # "learning_rate": 5e-4,
+    "gamma": 0.99,
+}
+
+model = PPO("MlpPolicy", env, verbose=1, **params)
 # we need to handle episode termination manually since SB3 doesn't issues #284 and #633
 # model.learn(total_timesteps=1000)
 
@@ -22,7 +32,7 @@ class MWTerminationCallback(BaseCallback):
 
     def __init__(self, verbose = 0):
         super(MWTerminationCallback, self).__init__(verbose)
-        self.step_count = 0
+        self.path_length = 0
 
     def _on_training_start(self):
         pass
@@ -31,6 +41,13 @@ class MWTerminationCallback(BaseCallback):
         pass
 
     def _on_step(self) -> bool:
+        # TODO: Check this for safety such that returns are not
+        # estimated across multiple episodes via discount factor
+        self.path_length += 1
+
+        if self.path_length >= 500:
+            self.model.env.reset()
+            self.path_length = 0
         return True
 
     def _on_rollout_end(self):
@@ -43,36 +60,71 @@ class MWTerminationCallback(BaseCallback):
     def _on_training_end(self):
         pass
 
-def evaluate_model(model):
+def evaluate_model(model, T=10):
+    """
+    Runs the model over T total evaluations and gets the average
+    total return
+    """
+    rewards = 0
+    for i in range(T):
+        vec_env = model.get_env()
+        obs = vec_env.reset()
+        N = 500
+        for i in range(N):
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, info = vec_env.step(action)
+            rewards += reward
+            # seems to render to an offscreen window which we can't see
+            # correctly outputs rgb images we can construct into a video, though
+            # vec_env.render()
+
+    return rewards / T
+
+def render_model(model, file_name=task_name):
+    """
+    Runs a simulation of the task and renders a view of it to a gif
+    in the current working directory.
+    """
+    images = []
     vec_env = model.get_env()
+    img = vec_env.render(mode="rgb_array")
     obs = vec_env.reset()
     N = 500
-    rewards = 0
     for i in range(N):
+        images.append(img)
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, done, info = vec_env.step(action)
-        rewards += reward
         # seems to render to an offscreen window which we can't see
         # correctly outputs rgb images we can construct into a video, though
-        vec_env.render()
+        img = vec_env.render(mode="rgb_array")
 
-    env.close()
-    return rewards
+    imageio.mimsave(f"{file_name}.gif", [np.array(img) for i, img in enumerate(images) if i % 5 == 0])
+
 
 # manual learning
 total_timesteps = 1_000_000
 iteration = 0
 total_timesteps, callback = model._setup_learn(total_timesteps, callback=MWTerminationCallback())
 while model.num_timesteps < total_timesteps:
+    # periodically evaluate
+    if iteration % 10 == 0:
+        print(f"Iteration {iteration} ({model.num_timesteps} timesteps): {evaluate_model(model)}")
+
+    if iteration % 100 == 0:
+        print("Creating render")
+        render_model(model)
+
     model.get_env().reset()
     continue_training = model.collect_rollouts(model.env, callback, model.rollout_buffer, n_rollout_steps=model.n_steps)
 
     iteration += 1
-    
-    # periodically evaluate
-    if iteration % 10 == 0:
-        print(f"Iteration {iteration}: {evaluate_model(model)}")
 
     model.train()
 
+
 print(f"Completed {iteration} iterations")
+
+print(evaluate_model(model))
+render_model(model)
+
+env.close()
