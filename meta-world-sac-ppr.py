632fa313f81a0d6c980b9ba449bc988d8d8e632d
Arpan Dhatt <ary.dhatt@gmail.com>
2023-03-02 22:32:47 -0600

    new sac ppr code using custom rollout collector

--- meta-world-sac-ppr.py
+++ meta-world-sac-ppr.py
@@ -60,7 +60,7 @@ params = {
     "use_sde": False,
     'policy_kwargs': policy_kwargs,
     'tau': 5e-3,
-    'buffer_size': 1000000,
+    'buffer_size': 100000,
     # has massive impact on training speed.
     'train_freq': (100, 'step'),
     'device': 'cpu',
@@ -68,19 +68,20 @@ params = {
 
 model = SAC("MlpPolicy", env, verbose = 1, **params)
 
-# load past policy and join environment/replay_buffer
-past_model = SAC.load(past_policy_path)
-past_model.env = model.env
-past_model.replay_buffer = model.replay_buffer
+# create past policy model and load parameters
+past_model = SAC("MlpPolicy", env, verbose = 1, **params)
+infile = open(past_policy_path, "rb")
+loaded_params = pickle.load(infile)
+infile.close()
 
-class MWTerminationCallback(BaseCallback):
-    """
-    A custom callback that terminates the rollout procedure early when we've reached the maximum
-    steps of the metaworld environment.
-    """
+past_model.set_parameters(loaded_params)
 
+class EmptyCallback(BaseCallback):
+    """
+    doesn't do anything anymore
+    """
     def __init__(self, verbose = 0):
-        super(MWTerminationCallback, self).__init__(verbose)
+        super(EmptyCallback, self).__init__(verbose)
         self.path_length = 0
 
     def _on_training_start(self):
@@ -90,26 +91,13 @@ class MWTerminationCallback(BaseCallback):
         pass
 
     def _on_step(self) -> bool:
-        # TODO: Check this for safety such that returns are not
-        # estimated across multiple episodes via discount factor
-        # self.path_length += 1
-        #
-        # if self.path_length >= 500:
-        #     self.model.env.reset()
-        #     self.path_length = 0
         return True
 
     def _on_rollout_end(self):
-        """
-        Manually reset environment since MW won't do it itself
-        """
-        self.model.env.reset()
         pass
 
     def _on_training_end(self):
         pass
-
-
 
 def evaluate_model(model, T=2):
     """
@@ -157,12 +145,139 @@ def render_model(model, file_name=task_name):
     os.system(f'ffmpeg -i {path}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" {path}.mp4'  + ' > /dev/null 2>&1')
     os.system(f'rm {path}.gif')
 
+def collect_ppr_rollouts(
+    self,
+    past_model,
+    reuse_prob,
+    env,
+    callback,
+    train_freq,
+    replay_buffer,
+    action_noise = None,
+    learning_starts = 0,
+    log_interval = None,
+):
+    from gym import spaces
+    from stable_baselines3.common.vec_env import VecEnv
+    from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, RolloutReturn, Schedule, TrainFreq, TrainFrequencyUnit
+    from stable_baselines3.common.utils import safe_mean, should_collect_more_steps
+    """
+    Collect experiences and store them into a ``ReplayBuffer``.
+    :param env: The training environment
+    :param callback: Callback that will be called at each step
+        (and at the beginning and end of the rollout)
+    :param train_freq: How much experience to collect
+        by doing rollouts of current policy.
+        Either ``TrainFreq(<n>, TrainFrequencyUnit.STEP)``
+        or ``TrainFreq(<n>, TrainFrequencyUnit.EPISODE)``
+        with ``<n>`` being an integer greater than 0.
+    :param action_noise: Action noise that will be used for exploration
+        Required for deterministic policy (e.g. TD3). This can also be used
+        in addition to the stochastic policy for SAC.
+    :param learning_starts: Number of steps before learning for the warm-up phase.
+    :param replay_buffer:
+    :param log_interval: Log data every ``log_interval`` episodes
+    :return:
+    """
+    # Switch to eval mode (this affects batch norm / dropout)
+    self.policy.set_training_mode(False)
+
+    num_collected_steps, num_collected_episodes = 0, 0
+
+    assert isinstance(env, VecEnv), "You must pass a VecEnv"
+    assert train_freq.frequency > 0, "Should at least collect one step or episode."
+
+    if env.num_envs > 1:
+        assert train_freq.unit == TrainFrequencyUnit.STEP, "You must use only one env when doing episodic training."
+
+    # Vectorize action noise if needed
+    if action_noise is not None and env.num_envs > 1 and not isinstance(action_noise, VectorizedActionNoise):
+        action_noise = VectorizedActionNoise(action_noise, env.num_envs)
+
+    if self.use_sde:
+        self.actor.reset_noise(env.num_envs)
+
+    callback.on_rollout_start()
+    continue_training = True
+
+    while should_collect_more_steps(train_freq, num_collected_steps, num_collected_episodes):
+        if self.use_sde and self.sde_sample_freq > 0 and num_collected_steps % self.sde_sample_freq == 0:
+            # Sample a new noise matrix
+            self.actor.reset_noise(env.num_envs)
+
+        # Sample action from learning policy or from past policy
+        actions, buffer_actions = None, None
+        if random.random() < reuse_prob:
+            # take the action the past model would do
+            unscaled_action, _ = past_model.predict(self._last_obs, deterministic=True)
+
+            # Rescale the action from [low, high] to [-1, 1]
+            if isinstance(self.action_space, spaces.Box):
+                scaled_action = self.policy.scale_action(unscaled_action)
+
+                # Add noise to the action (improve exploration)
+                if action_noise is not None:
+                    scaled_action = np.clip(scaled_action + action_noise(), -1, 1)
+
+                # We store the scaled action in the buffer
+                buffer_actions = scaled_action
+                actions = self.policy.unscale_action(scaled_action)
+            else:
+                # Discrete case, no need to normalize or clip
+                buffer_actions = unscaled_action
+                actions = buffer_actions
+        else:
+            actions, buffer_actions = self._sample_action(learning_starts, action_noise, env.num_envs)
+
+        # Rescale and perform action
+        new_obs, rewards, dones, infos = env.step(actions)
+
+        self.num_timesteps += env.num_envs
+        num_collected_steps += 1
+
+        # Give access to local variables
+        callback.update_locals(locals())
+        # Only stop training if return value is False, not when it is None.
+        if callback.on_step() is False:
+            return RolloutReturn(num_collected_steps * env.num_envs, num_collected_episodes, continue_training=False)
+
+        # Retrieve reward and episode length if using Monitor wrapper
+        self._update_info_buffer(infos, dones)
+
+        # Store data in replay buffer (normalized action and unnormalized observation)
+        self._store_transition(replay_buffer, buffer_actions, new_obs, rewards, dones, infos)
+
+        self._update_current_progress_remaining(self.num_timesteps, self._total_timesteps)
+
+        # For DQN, check if the target network should be updated
+        # and update the exploration schedule
+        # For SAC/TD3, the update is dones as the same time as the gradient update
+        # see https://github.com/hill-a/stable-baselines/issues/900
+        self._on_step()
+
+        for idx, done in enumerate(dones):
+            if done:
+                # Update stats
+                num_collected_episodes += 1
+                self._episode_num += 1
+
+                if action_noise is not None:
+                    kwargs = dict(indices=[idx]) if env.num_envs > 1 else {}
+                    action_noise.reset(**kwargs)
+
+                # Log training infos
+                if log_interval is not None and self._episode_num % log_interval == 0:
+                    self._dump_logs()
+    callback.on_rollout_end()
+
+    return RolloutReturn(num_collected_steps * env.num_envs, num_collected_episodes, continue_training)
+
 x_time_steps = []
 y_total_reward = []
 y_success_rate = []
 
 total_timesteps = 3_000_000
-total_timesteps, callback = model._setup_learn(total_timesteps, callback = MWTerminationCallback())
+total_timesteps, callback = model._setup_learn(total_timesteps, callback = EmptyCallback())
 reuse_prob = init_reuse_prob
 
 while model.num_timesteps < total_timesteps:
@@ -187,26 +302,15 @@ while model.num_timesteps < total_timesteps:
         #     
     model.get_env().reset()
 
-    # artificially collect train_freq steps
-    used_old_policy = 0
-    for i in range(model.train_freq.frequency):
-        if random.random() < reuse_prob:
-            # use past policy
-            past_model.collect_rollouts(model.env, callback, TrainFreq(1, TrainFrequencyUnit.STEP), replay_buffer = model.replay_buffer)
-            model.num_timesteps += 1
-            used_old_policy += 1
-        else:
-            # use new policy
-            model.collect_rollouts(model.env, callback, TrainFreq(1, TrainFrequencyUnit.STEP), replay_buffer = model.replay_buffer)
-        reuse_prob *= reuse_decay
-
+    # collect experience using PPR
+    collect_ppr_rollouts(model, past_model, reuse_prob, model.env, callback, model.train_freq, replay_buffer = model.replay_buffer)
 
     if model.num_timesteps > model.learning_starts:
         model.train(gradient_steps = 250)
         if model.num_timesteps % 10000 == 0:
             render_model(model, file_name=task_name+"-s{:07d}-r{}".format(model.num_timesteps, total_reward[0]))
 
-    print(f"taken {time.time() - start_time} seconds; used old policy {used_old_policy/model.train_freq.frequency*100:.2f}% of the time")
+    print(f"taken {time.time() - start_time} seconds")
 
 
 x_time_steps.append(model.num_timesteps)
