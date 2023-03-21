9061f41920bedd1d0cf29dbad929a7982b207e26
Omeed Tehrani <omeed@cs.utexas.edu>
2023-03-21 16:21:11 -0500

    switching back to wandb

--- meta-world-sac.py
+++ meta-world-sac.py
@@ -24,11 +24,12 @@ from enum import Enum
 from TimeLimit import TimeLimit
 import sys
 import time
+import wandb
 
-# switching from wandb to tensorboard
-import torch
-from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter()
+# link to wandb.
+wandb.init(
+    project="robin-research-theoretical-similarity",
+)
 
 task_name = sys.argv[1]
 # task_name = 'pick-and-place-v1'
@@ -198,7 +199,11 @@ while model.num_timesteps < total_timesteps:
         plt.xlabel("Timesteps Trained")
         plt.ylabel("Total Reward per Episode")
         plt.savefig(curve_png)
-        writer.add_scalar("Timesteps Trained/Total Reward Per Episode", x_time_steps[-1], y_total_reward[-1])
+        wandb.log({
+            "Timesteps Trained": x_time_steps[-1], 
+            "Total Reward per Episode": y_total_reward[-1]
+                })
+            
     model.get_env().reset()
     continue_training = model.collect_rollouts(model.env, callback, model.train_freq, replay_buffer = model.replay_buffer)
 
@@ -211,15 +216,12 @@ while model.num_timesteps < total_timesteps:
     print(f"Iteration {iteration} taken {time.time() - start_time} seconds")
     iteration += 1
 
-writer.flush()
-
 
 x_time_steps.append(model.num_timesteps)
 total_reward = evaluate_model(model)[0]
 y_total_reward.append(total_reward)
 print(f"Iteration {iteration} ({model.num_timesteps} timesteps): {total_reward}")
 render_model(model, file_name=task_name+"-s{:07d}-r{}".format(model.num_timesteps, total_reward))
-wrapped_env.close()
-writer.close()
 
+wrapped_env.close()
 
