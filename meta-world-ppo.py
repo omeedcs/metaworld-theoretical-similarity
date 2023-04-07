46eb0bd2758fed6b600bd9b7b5176d524cc4be0f
Arpan Dhatt <ary.dhatt@gmail.com>
2023-04-07 17:11:46 -0500

    add ppo to rllib and add some (so far unsuccessful) sac params

--- meta-world-ppo.py
+++ meta-world-ppo.py
@@ -1,151 +1,58 @@
-from typing import Callable
-import random
-import os
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.tune.logger import pretty_print
+from ray.tune.registry import register_env
+from ray.rllib.models.catalog import ModelCatalog, ModelConfigDict
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from ray.rllib.evaluation.episode_v2 import EpisodeV2
+from ray.rllib.evaluation.episode import Episode
+from ray.rllib.evaluation import RolloutWorker
+from ray.rllib.env.base_env import BaseEnv
+from gymnasium.wrappers import EnvCompatibility
+from MWSamplingEnvironment import MWSamplingEnvironment
 
-import matplotlib.pyplot as plt
-from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import BaseCallback
-import metaworld
-import imageio
-import numpy as np
+import sys
+from typing import Union
 
+TASK_NAME = "button-press-v2"
 
-task_name = 'pick-place-v2'
-video_dir = 'training_mp4s' 
+def env_creator(env_config):
+    env = EnvCompatibility(MWSamplingEnvironment(TASK_NAME))
 
-os.system(f"rm -r {video_dir}")
+    return env
 
-ml1 = metaworld.ML1(task_name)
+register_env(TASK_NAME, env_creator)
 
-env = ml1.train_classes[task_name]()
-task = ml1.train_tasks[0]
-env.set_task(task)
-
-params = {
-    # "learning_rate": 5e-4,
-    "ent_coef": 5e-4
-}
-
-model = PPO("MlpPolicy", env, verbose=1, **params)
-# we need to handle episode termination manually since SB3 doesn't issues #284 and #633
-# model.learn(total_timesteps=1000)
-
-class MWTerminationCallback(BaseCallback):
-    """
-    A custom callback that terminates the rollout procedure early when we've reached the maximum
-    steps of the metaworld environment.
-    """
-
-    def __init__(self, verbose = 0):
-        super(MWTerminationCallback, self).__init__(verbose)
-        self.path_length = 0
-
-    def _on_training_start(self):
-        pass
-
-    def _on_rollout_start(self):
-        pass
-
-    def _on_step(self) -> bool:
-        # TODO: Check this for safety such that returns are not
-        # estimated across multiple episodes via discount factor
-        self.path_length += 1
-
-        if self.path_length >= 500:
-            self.model.env.reset()
-            self.path_length = 0
-        return True
-
-    def _on_rollout_end(self):
-        """
-        Manually reset environment since MW won't do it itself
-        """
-        self.model.env.reset()
-        pass
-
-    def _on_training_end(self):
-        pass
-
-def evaluate_model(model, T=2):
-    """
-    Runs the model over T total evaluations and gets the average
-    total return
-    """
-    rewards = 0
-    for i in range(T):
-        vec_env = model.get_env()
-        obs = vec_env.reset()
-        N = 500
-        for i in range(N):
-            action, _states = model.predict(obs, deterministic=True)
-            obs, reward, done, info = vec_env.step(action)
-            rewards += reward
-            # seems to render to an offscreen window which we can't see
-            # correctly outputs rgb images we can construct into a video, though
-            # vec_env.render()
-
-    return rewards / T
-
-def render_model(model, file_name=task_name):
-    """
-    Runs a simulation of the task and renders a view of it to a gif
-    in the current working directory.
-    """
-    print("Creating render")
-    images = []
-    vec_env = model.get_env()
-    img = vec_env.render(mode="rgb_array")
-    obs = vec_env.reset()
-    N = 500
-    for i in range(N):
-        images.append(img)
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, done, info = vec_env.step(action)
-        # seems to render to an offscreen window which we can't see
-        # correctly outputs rgb images we can construct into a video, though
-        img = vec_env.render(mode="rgb_array")
-
-    if not os.path.isdir(f"{video_dir}"):
-        os.mkdir(video_dir)
-    path = os.path.join(video_dir, f"{file_name}")
-    imageio.mimsave(path+".gif", [np.array(img) for i, img in enumerate(images) if i % 5 == 0])
-    os.system(f'ffmpeg -i {path}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" {path}.mp4')
-    os.system(f'rm {path}.gif')
-
-x_time_steps = []
-y_total_reward = []
-
-# manual learning
-total_timesteps = 5_000_000
-iteration = 0
-total_timesteps, callback = model._setup_learn(total_timesteps, callback=MWTerminationCallback())
-while model.num_timesteps < total_timesteps:
-    # periodically evaluate
-    if iteration % 25 == 0:
-        total_reward = evaluate_model(model)[0]
-        x_time_steps.append(model.num_timesteps)
-        y_total_reward.append(total_reward)
-        print(f"Iteration {iteration} ({model.num_timesteps} timesteps): {total_reward}")
-        render_model(model, file_name=task_name+"-s{:07d}-r{}".format(model.num_timesteps, total_reward))
-
-        plt.plot(x_time_steps, y_total_reward)
-        plt.xlabel("Timesteps Trained")
-        plt.ylabel("Total Reward per Episode")
-        plt.savefig("training_curve.png")
-
-    model.get_env().reset()
-    continue_training = model.collect_rollouts(model.env, callback, model.rollout_buffer, n_rollout_steps=model.n_steps)
-
-    iteration += 1
-
-    model.train()
+class SuccessMetric(DefaultCallbacks):
+    def on_episode_step(self, *, worker: RolloutWorker, base_env: BaseEnv, policies, episode: EpisodeV2, env_index, **kwargs):
+        agent_id = list(base_env.get_agent_ids())[0]
+        episode.custom_metrics = episode._last_infos[agent_id]
 
 
-x_time_steps.append(model.num_timesteps)
-total_reward = evaluate_model(model)[0]
-y_total_reward.append(total_reward)
-print(f"Iteration {iteration} ({model.num_timesteps} timesteps): {total_reward}")
-render_model(model, file_name=task_name+"-s{:07d}-r{}".format(model.num_timesteps, total_reward))
+algo = (
+    PPOConfig()
+    .callbacks(SuccessMetric)
+    .training(
+        model=ModelConfigDict(
+            fcnet_hiddens =  [512, 512],
+            fcnet_activation = "tanh"
+        )
+    )
+    .framework("torch")
+    .rollouts(
+        num_rollout_workers=4
+    )
+    .resources(
+        num_gpus=0, 
+        num_cpus_per_worker=1
+    )
+    .environment(env = TASK_NAME)
+    .build()
+)
 
-env.close()
+for i in range(20000):
+    result = algo.train()
+    print(pretty_print(result))
 
+    if i % 250 == 0:
+        checkpoint_dir = algo.save()
+        print(f"Checkpoint saved in directory {checkpoint_dir}")
--- meta-world-sac.py
+++ meta-world-sac.py
@@ -2,11 +2,19 @@ from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.algorithms.sac import SACConfig
 from ray.tune.logger import pretty_print
 from ray.tune.registry import register_env
+from ray.rllib.models.catalog import ModelCatalog, ModelConfigDict
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from ray.rllib.evaluation.episode_v2 import EpisodeV2
+from ray.rllib.evaluation.episode import Episode
+from ray.rllib.evaluation import RolloutWorker
+from ray.rllib.env.base_env import BaseEnv
 from gymnasium.wrappers import EnvCompatibility
 from MWSamplingEnvironment import MWSamplingEnvironment
-import sys
 
-TASK_NAME = "bin-picking-v2"
+import sys
+from typing import Union
+
+TASK_NAME = "pick-place-v2"
 
 def env_creator(env_config):
     env = EnvCompatibility(MWSamplingEnvironment(TASK_NAME))
@@ -15,11 +23,32 @@ def env_creator(env_config):
 
 register_env(TASK_NAME, env_creator)
 
+class SuccessMetric(DefaultCallbacks):
+    def on_episode_step(self, *, worker: RolloutWorker, base_env: BaseEnv, policies, episode: EpisodeV2, env_index, **kwargs):
+        agent_id = list(base_env.get_agent_ids())[0]
+        episode.custom_metrics = episode._last_infos[agent_id]
+
+
 algo = (
     SACConfig()
+    .callbacks(SuccessMetric)
+    .training(
+        lr=3e-4,
+        gamma=0.99,
+        tau=5e-3,
+        model=ModelConfigDict(
+            fcnet_hiddens =  [400, 400],
+            fcnet_activation = "relu"
+        )
+    )
     .framework("torch")
-    .rollouts(num_rollout_workers=4)
-    .resources(num_gpus=0, num_cpus_per_worker=1)
+    .rollouts(
+        num_rollout_workers=4
+    )
+    .resources(
+        num_gpus=0, 
+        num_cpus_per_worker=1
+    )
     .environment(env = TASK_NAME)
     .build()
 )
