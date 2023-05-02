90393d02b29f3519b8a1d88289ac966edc51fdb5
Haotian Wang <46818795+DDZRNL@users.noreply.github.com>
2023-05-02 00:56:36 -0500

    Add files via upload

---
+++ MWSamplingEnvironment.py
@@ -0,0 +1,86 @@
+"""Wrapper for limiting the time steps of an environment."""
+from typing import Optional
+
+import gym
+import random
+from metaworld.envs import ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE
+from gymnasium import spaces
+import numpy as np
+
+
+class MWSamplingEnvironment(gym.Wrapper):
+
+    """This wrapper will issue a `truncated` signal if a maximum number of timesteps is exceeded.
+    If a truncation is not defined inside the environment itself, this is the only place that the truncation signal is issued.
+    Critically, this is different from the `terminated` signal that originates from the underlying environment as part of the MDP.
+    Example:
+       >>> from gym.envs.classic_control import CartPoleEnv
+       >>> from gym.wrappers import TimeLimit
+       >>> env = CartPoleEnv()
+       >>> env = TimeLimit(env, max_episode_steps=1000)
+    """
+
+    def __init__(
+        self,
+        task_name: str
+    ):
+        """Initializes the :class:`TimeLimit` wrapper with an environment and the number of steps after which truncation will occur.
+        Args:
+            env: The environment to apply the wrapper
+            max_episode_steps: An optional max episode steps (if ``Ǹone``, ``env.spec.max_episode_steps`` is used)
+        """
+        max_episode_steps = 500
+        self.env_constructor = ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE[f'{task_name}-goal-observable']
+
+        super().__init__(self.sample_env())
+        if max_episode_steps is None and self.env.spec is not None:
+            max_episode_steps = self.env.spec.max_episode_steps
+        if self.env.spec is not None:
+            self.env.spec.max_episode_steps = max_episode_steps
+        self._max_episode_steps = max_episode_steps
+        self._elapsed_steps = None
+
+    def sample_env(self):
+        new_env = self.env_constructor(seed=random.randint(0, 10000))
+        new_env.reset()
+
+        return new_env
+
+    @property
+    def observation_space(self):
+        return spaces.Box(low=self.env.observation_space.low, high=self.env.observation_space.high, dtype=self.env.observation_space.dtype)
+
+    @property
+    def action_space(self):
+        return spaces.Box(low=self.env.action_space.low, high=self.env.action_space.high, dtype=self.env.action_space.dtype)
+
+    def step(self, action):
+        """Steps through the environment and if the number of steps elapsed exceeds ``max_episode_steps`` then truncate.
+        Args:
+            action: The environment step action
+        Returns:
+            The environment step ``(observation, reward, terminated, truncated, info)`` with `truncated=True`
+            if the number of steps elapsed >= max episode steps
+        """
+        observation, reward, done, info = self.env.step(action)
+        self._elapsed_steps += 1
+
+        if self._elapsed_steps >= self._max_episode_steps or info["success"] == True:
+            reward *= (self._max_episode_steps - self._elapsed_steps) * 0.5
+            done = True
+        
+        if((self.env.observation_space.low > observation).any() or (self.env.observation_space.high < observation).any()):
+            done = True
+
+        return observation, reward, done, info
+
+    def reset(self, *args, seed=None, options=None):
+        """Resets the environment with :param:`**kwargs` and sets the number of steps elapsed to zero.
+        Args:
+            **kwargs: The kwargs to reset the environment with
+        Returns:
+            The reset environment
+        """
+        self._elapsed_steps = 0
+        self.env = self.sample_env()
+        return self.env.reset(*args)
--- meta-world-ppo.py
+++ meta-world-ppo.py
@@ -1,151 +1,81 @@
-from typing import Callable
-import random
-import os
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.tune.logger import pretty_print
+from ray.tune.registry import register_env
+from ray.rllib.models.catalog import ModelCatalog, ModelConfigDict
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from ray.rllib.evaluation.episode_v2 import EpisodeV2
+from ray.rllib.evaluation.episode import Episode
+from ray.rllib.evaluation import RolloutWorker
+from ray.rllib.env.base_env import BaseEnv
+from gymnasium.wrappers import EnvCompatibility
+from MWSamplingEnvironment import MWSamplingEnvironment
 
-import matplotlib.pyplot as plt
-from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import BaseCallback
-import metaworld
-import imageio
-import numpy as np
+import sys
+from typing import Union
 
+import torch
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
 
-task_name = 'pick-place-v2'
-video_dir = 'training_mp4s' 
+TASK_NAME = "soccer-v2"
 
-os.system(f"rm -r {video_dir}")
+writer = SummaryWriter(f"./runs/{TASK_NAME}")
 
-ml1 = metaworld.ML1(task_name)
+def env_creator(env_config):
+    env = EnvCompatibility(MWSamplingEnvironment(TASK_NAME))
+    return env
 
-env = ml1.train_classes[task_name]()
-task = ml1.train_tasks[0]
-env.set_task(task)
+register_env(TASK_NAME, env_creator)
 
-params = {
-    # "learning_rate": 5e-4,
-    "ent_coef": 5e-4
-}
-
-model = PPO("MlpPolicy", env, verbose=1, **params)
-# we need to handle episode termination manually since SB3 doesn't issues #284 and #633
-# model.learn(total_timesteps=1000)
-
-class MWTerminationCallback(BaseCallback):
-    """
-    A custom callback that terminates the rollout procedure early when we've reached the maximum
-    steps of the metaworld environment.
-    """
-
-    def __init__(self, verbose = 0):
-        super(MWTerminationCallback, self).__init__(verbose)
-        self.path_length = 0
-
-    def _on_training_start(self):
-        pass
-
-    def _on_rollout_start(self):
-        pass
-
-    def _on_step(self) -> bool:
-        # TODO: Check this for safety such that returns are not
-        # estimated across multiple episodes via discount factor
-        self.path_length += 1
-
-        if self.path_length >= 500:
-            self.model.env.reset()
-            self.path_length = 0
-        return True
-
-    def _on_rollout_end(self):
-        """
-        Manually reset environment since MW won't do it itself
-        """
-        self.model.env.reset()
-        pass
-
-    def _on_training_end(self):
-        pass
-
-def evaluate_model(model, T=2):
-    """
-    Runs the model over T total evaluations and gets the average
-    total return
-    """
-    rewards = 0
-    for i in range(T):
-        vec_env = model.get_env()
-        obs = vec_env.reset()
-        N = 500
-        for i in range(N):
-            action, _states = model.predict(obs, deterministic=True)
-            obs, reward, done, info = vec_env.step(action)
-            rewards += reward
-            # seems to render to an offscreen window which we can't see
-            # correctly outputs rgb images we can construct into a video, though
-            # vec_env.render()
-
-    return rewards / T
-
-def render_model(model, file_name=task_name):
-    """
-    Runs a simulation of the task and renders a view of it to a gif
-    in the current working directory.
-    """
-    print("Creating render")
-    images = []
-    vec_env = model.get_env()
-    img = vec_env.render(mode="rgb_array")
-    obs = vec_env.reset()
-    N = 500
-    for i in range(N):
-        images.append(img)
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, done, info = vec_env.step(action)
-        # seems to render to an offscreen window which we can't see
-        # correctly outputs rgb images we can construct into a video, though
-        img = vec_env.render(mode="rgb_array")
-
-    if not os.path.isdir(f"{video_dir}"):
-        os.mkdir(video_dir)
-    path = os.path.join(video_dir, f"{file_name}")
-    imageio.mimsave(path+".gif", [np.array(img) for i, img in enumerate(images) if i % 5 == 0])
-    os.system(f'ffmpeg -i {path}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" {path}.mp4')
-    os.system(f'rm {path}.gif')
-
-x_time_steps = []
-y_total_reward = []
-
-# manual learning
-total_timesteps = 5_000_000
-iteration = 0
-total_timesteps, callback = model._setup_learn(total_timesteps, callback=MWTerminationCallback())
-while model.num_timesteps < total_timesteps:
-    # periodically evaluate
-    if iteration % 25 == 0:
-        total_reward = evaluate_model(model)[0]
-        x_time_steps.append(model.num_timesteps)
-        y_total_reward.append(total_reward)
-        print(f"Iteration {iteration} ({model.num_timesteps} timesteps): {total_reward}")
-        render_model(model, file_name=task_name+"-s{:07d}-r{}".format(model.num_timesteps, total_reward))
-
-        plt.plot(x_time_steps, y_total_reward)
-        plt.xlabel("Timesteps Trained")
-        plt.ylabel("Total Reward per Episode")
-        plt.savefig("training_curve.png")
-
-    model.get_env().reset()
-    continue_training = model.collect_rollouts(model.env, callback, model.rollout_buffer, n_rollout_steps=model.n_steps)
-
-    iteration += 1
-
-    model.train()
+class SuccessMetric(DefaultCallbacks):
+    def on_episode_step(self, *, worker: RolloutWorker, base_env: BaseEnv, policies, episode: EpisodeV2, env_index, **kwargs):
+        agent_id = list(base_env.get_agent_ids())[0]
+        episode.custom_metrics = episode._last_infos[agent_id]
 
 
-x_time_steps.append(model.num_timesteps)
-total_reward = evaluate_model(model)[0]
-y_total_reward.append(total_reward)
-print(f"Iteration {iteration} ({model.num_timesteps} timesteps): {total_reward}")
-render_model(model, file_name=task_name+"-s{:07d}-r{}".format(model.num_timesteps, total_reward))
+algo = (
+    PPOConfig()
+    .callbacks(SuccessMetric)
+    .training(
+        model=ModelConfigDict(
+            fcnet_hiddens =  [512, 512],
+            fcnet_activation = "tanh"
+        )
+    )
+    .framework("torch")
+    .rollouts(
+        num_rollout_workers=1
+    )
+    .resources(
+        num_gpus=1, 
+        num_gpus_per_worker=1
+    )
+    .environment(env = TASK_NAME)
+    .build()
+)
 
-env.close()
+num_episodes = 1000
+success_episodes = 0
 
+for i in range(num_episodes):
+    result = algo.train()
+    # print(pretty_print(result))
+    # print(pretty_print(result['custom_metrics']))
+    if(result['custom_metrics']['success_mean'] > 0):
+        success_episodes += 1
+        print("The success times is: ", success_episodes)
+
+    writer.add_scalar("episode_reward_mean/train", result['episode_reward_mean'], i)
+    success_rate = round(success_episodes/float(i+1.0), 5)
+    writer.add_scalar("success rate/train", success_rate, i)
+
+    if i % 250 == 0:
+        checkpoint_dir = algo.save(f"./Checkpoints/{TASK_NAME}/")
+        print(f"Checkpoint saved in directory {checkpoint_dir}")
+
+# save the policy
+policy = algo.get_policy()
+torch.save(policy.model.state_dict(), f"./saved_policies/{TASK_NAME}_policy.pth")
+
+writer.flush()
+writer.close()
